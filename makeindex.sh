3432e285f86f6f9a2a9b57c527ba82bb6e15c660
mvermeer <mvermeer>
2001-03-22 05:51:49 +0000

    This is patching up makeindex.sh to work with multiple languages. Only finnish and english supported at this stage.
    The <dp>-<encoding>.xdy file in the language directory should now
    contain a require to an <encoding>.xdy file in inputenc. No 'root'
    files necessary.

--- makeindex.sh
+++ makeindex.sh
@@ -4,11 +4,13 @@
 if [ "$1" = "" ] ; then
   echo ""
   echo "Syntax:" 
-  echo "  ./makeindex.sh [-m <language> [-v <variant>]] [-d <docprocessor>] [-s <style>] <myindex>"
+  echo "  ./makeindex.sh [[-v <var>] [-e <enc>] -m <lang>] [-d <dp>] [-s <style>] <myindex>"
   echo ""
   echo "  where <myindex> is the index file name *without* extension."
   echo ""
-  echo "Languages:"
+  echo "Multiple -m options allowed; -m always after corresponding -v, -e."
+  echo ""
+  echo "Languages <lang>:"
   echo "  belarusian  finnish         swedish"
   echo "  bulgarian   french          norwegian"
   echo "  croatian    general         polish"
@@ -17,49 +19,50 @@ if [ "$1" = "" ] ; then
   echo "  english     slovak          ukrainian"
   echo "  esperanto   macedonian      spanish"
   echo ""
-  echo "Variants:"
+  echo "Variants <var>:"
   echo "  german:  duden, din5007"
   echo "  spanish: traditional, modern"
   echo "  slovak:  large, small"
   echo ""
-  echo "Docprocessors:"
+  echo "Docprocessors <dp>:"
   echo "  -t :    -d tex"
   echo "  -n :    -d nroff"
   echo ""
-  echo "Styles:"
+  echo "Styles <style>:"
   echo "  -s <style>"
   echo "  -s makeindex (default)"
   exit 0
 fi
 
-LOC="english"
-LOCALE=NO
-PRE="en"
-
+LOC=""
+ENC=""
 VAR=""
+
+TEMP=`mktemp /tmp/xindy.XXXXXX` || exit 1
+
+echo "" >$TEMP.xdy
+
 DP="tex"
 STYLE="styles/makeindex.xdy"
-ENC="ascii"
 
 while getopts "m:v:d:e:tnls:" OPT
 do
   case "$OPT" in
     "m")
       LOC="$OPTARG"
-      LOCALE=YES
       case $LOC in
         "english")
-        ENC="ascii"
-	PRE="en"
+        ENC_="ascii"
+        PRE="en"
         ;;
         "finnish")
-        ENC="cp1252"
+        ENC_="cp1252"
         PRE="fi"
         ;;
         "spanish")
-        ENC="latin1"
+        ENC_="latin1"
         PRE="es"
-        VAR="traditional-"
+        VAR_="traditional-"
         ;;
       esac
       IGNORESPECIAL="$IGNORESPECIAL \"$PRE-ignore-special\"" 
@@ -67,6 +70,18 @@ do
       RESOLVEDIACRITICS="$RESOLVEDIACRITICS \"$PRE-resolve-diacritics\"" 
       RESOLVECASE="$RESOLVECASE \"$PRE-resolve-case\"" 
       RESOLVESPECIAL="$RESOLVESPECIAL \"$PRE-resolve-special\"" 
+      if [ "$ENC" = "" ] ; then
+        ENC=$ENC_  
+      fi
+      if [ "$VAR" = "" ] ; then
+        VAR=$VAR_  
+      fi
+
+      # Create file containing both locale and style req's: 
+      echo "(require \"lang/$LOC/$DP-$VAR$ENC.xdy\")" >>$TEMP.xdy
+      echo "(require \"lang/$LOC/$VAR$ENC.xdy\")" >>$TEMP.xdy
+      ENC=""
+      VAR=""
       ;;
     "v")
       VAR="$OPTARG-"
@@ -94,12 +109,19 @@ done
 
 
 #
-if [ "$LOCALE" = "NO" ] ; then
+if [ "$LOC" = "" ] ; then
+  LOC="english"
+  ENC="ascii"
+  PRE="en"
   IGNORESPECIAL="\"en-ignore-special\""
   ALPHABETIZE="\"en-alphabetize\""
   RESOLVEDIACRITICS="\"en-resolve-diacritics\"" 
   RESOLVECASE="\"en-resolve-case\"" 
   RESOLVESPECIAL="\"en-resolve-special\"" 
+  # Do this in the absence of -m options:
+  echo "(require \"lang/$LOC/$DP-$VAR$ENC.xdy\")" >>$TEMP.xdy
+  echo "(require \"lang/$LOC/$VAR$ENC.xdy\")" >>$TEMP.xdy
+
 fi
 
 shift $[$#-1]
@@ -107,21 +129,18 @@ DATA=$1
 
 tex2xindy <$DATA.idx >$DATA.raw
 
-# Create file containing both locale and style req's: 
-echo "(require \"lang/$LOC/$DP-$VAR$ENC.xdy\")" >$DATA.xdy
-echo "(require \"lang/$LOC/$VAR$ENC.xdy\")" >>$DATA.xdy
-
-echo "(define-sort-rule-orientations (forward backward forward forward))" >>$DATA.xdy
+echo "(define-sort-rule-orientations (forward backward forward forward))" \
+	>>$TEMP.xdy
 echo "(use-rule-set :run 0 :rule-set \
-	($ALPHABETIZE $IGNORESPECIAL))" >>$DATA.xdy
+	($ALPHABETIZE $IGNORESPECIAL))" >>$TEMP.xdy
 echo "(use-rule-set :run 1 :rule-set \
-	($RESOLVEDIACRITICS $IGNORESPECIAL))" >>$DATA.xdy
+	($RESOLVEDIACRITICS $IGNORESPECIAL))" >>$TEMP.xdy
 echo "(use-rule-set :run 2 :rule-set \
-	($RESOLVECASE $IGNORESPECIAL))" >>$DATA.xdy
+	($RESOLVECASE $IGNORESPECIAL))" >>$TEMP.xdy
 echo "(use-rule-set :run 3 :rule-set \
-	($RESOLVESPECIAL))" >>$DATA.xdy
+	($RESOLVESPECIAL))" >>$TEMP.xdy
 
-echo "(require \"$STYLE\")" >>$DATA.xdy
+echo "(require \"$STYLE\")" >>$TEMP.xdy
 
-xindy $DATA.xdy $DATA.raw
+xindy $TEMP.xdy $DATA.raw
 
